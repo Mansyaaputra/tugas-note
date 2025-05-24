c9b45955061942cf4cb81a304d4cd42494ffa356
Mansyaaputra <ajipeterrrr@gmail.com>
2025-05-24 18:50:44 +0700

    update

--- frontend/src/context/utils.js
+++ frontend/src/context/utils.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://notes-backend-mansya-663618957788.us-central1.run.app";
+export const BASE_URL = "https://notes-backend-mansya-663618957788.us-central1.run.app/";
 
 export const apiConfig = {
   headers: {
