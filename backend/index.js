1474cdae49caee5cb90fa5e59f871f4e647b961d
Mansyaaputra <ajipeterrrr@gmail.com>
2025-05-24 11:53:20 +0700

    update api

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ Konfigurasi CORS
 const allowedOrigins = [
-  "https://frontend-notes-mansya-dot-b-02-451105.uc.r.appspot.com",
+  "https://frontend-notes-putra-dot-g-09-450802.uc.r.appspot.com",
   "http://localhost:3000", // tambahkan ini agar development lokal tidak error CORS
 ];
 
